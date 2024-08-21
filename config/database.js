b1cd0385fd9cdb74644b589bbac4158aae9e3bea
josie-miller <josephinemiller13524@icloud.com>
2024-08-20 20:57:21 -0700

    hi

--- config/database.js
+++ config/database.js
@@ -1,7 +1,13 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
+const dbUrl = process.env.DATABASE_URL;
+
+if (!dbUrl) {
+  throw new Error("DATABASE_URL is not set");
+}
+
+const sequelize = new Sequelize(dbUrl, {
   dialect: 'postgres',
   protocol: 'postgres',
   logging: false,
